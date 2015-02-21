b85b4e0c506d06dc667bde0085e44dab57dc5cd2
lupyuen <lupyuen@gmail.com>
2015-02-21 10:37:31 +0800

    Autocommit

---
+++ MyApp/PictureListCell.swift
@@ -0,0 +1,13 @@
+//
+//  PictureListCell.swift
+//  MyApp
+//
+//  Created by Luppy on 21/2/15.
+//  Copyright (c) 2015 besqrddev. All rights reserved.
+//
+
+import UIKit
+
+class PictureListCell: UICollectionViewCell {
+    
+}
---
+++ MyApp/PictureListScreen.swift
@@ -0,0 +1,98 @@
+//
+//  PictureListScreen.swift
+//  MyApp
+//
+//  Created by Luppy on 21/2/15.
+//  Copyright (c) 2015 besqrddev. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "cell"
+
+class PictureListScreen: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    
+    let titles = ["Sand Harbor, Lake Tahoe - California",
+        "Beautiful View of Manhattan skyline.",
+        "Watcher in the Fog","Great Smoky Mountains National Park, Tennessee","Most beautiful place"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 1
+    }
+    
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return 50
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as PictureListCell
+        /*
+        cell.title.text = self.titles[indexPath.row % 5]
+        let curr = indexPath.row % 5  + 1
+        let imgName = "pin\(curr).jpg"
+        cell.pinImage.image = UIImage(named: imgName)
+        */
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView!,
+        layout collectionViewLayout: UICollectionViewLayout!,
+        sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
+            return CGSize(width: 170, height: 300)
+    }
+    
+    func collectionView(collectionView: UICollectionView!,
+        layout collectionViewLayout: UICollectionViewLayout!,
+        insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            return sectionInsets
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+    }
+    */
+    
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+    }
+    */
+    
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return false
+    }
+    
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+    return false
+    }
+    
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+    
+
+}
